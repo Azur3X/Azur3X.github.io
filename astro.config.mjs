9a897a492e86117797385974f26ef1c51b2296c1
AzureX (Mathias Bonde Jensen) <42809870+Azur3X@users.noreply.github.com>
2025-08-09 03:21:15 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://Azur3X.github.io", // your GitHub Pages URL or custom domain
-  base: "/", // use "/" for user/organization pages, or "/repo-name/" for project pages
   integrations: [tailwind()],
 });
